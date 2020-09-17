872c5fbb3d2b4b128f2ae853ca441036a325b9e5
Shona McKenzie <shona_mckenzie@hotmail.com>
2020-09-17 08:29:04 +0100

    added anagram solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,56 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function buildCharMap(str) {
+//   const charMap = {};
+//   for (const char of str) {
+//     if (!charMap[char]) {
+//       charMap[char] = 1;
+//     } else {
+//       charMap[char] ++;
+//     }
+//   }
+//   return charMap;
+// }
+
+function buildCharMap(str) {
+  const charMap = {};
+  for (const char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+  return charMap;
+}
+
+function anagrams(stringA, stringB) {
+  // stringA.replace(/[^\w]/g, '').toLowerCase();
+  // stringB.replace(/[^\w]/g, '').toLowerCase();
+  const charMapA = buildCharMap(stringA);
+  const charMapB = buildCharMap(stringB);
+
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false;
+  }
+  // for (const char of stringA) {
+  //   if (!charMapA[char]) {
+  //     charMapA[char] = 1;
+  //   } else {
+  //     charMapA[char] ++;
+  //   }
+  // }
+  // for (const char of stringB) {
+  //   if (!charMapB[char]) {
+  //     charMapB[char] = 1;
+  //   } else {
+  //     charMapB[char] ++;
+  //   }
+  // }
+  // eslint-disable-next-line no-restricted-syntax
+  for (const char in charMapA) {
+    if (charMapA[char] !== charMapB[char]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 module.exports = anagrams;
