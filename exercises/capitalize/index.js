644e780dd610a6b8a9358c8ea99cef77b718abd2
Shona McKenzie <shona_mckenzie@hotmail.com>
2020-09-19 12:59:22 +0100

    added capitalize string solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,29 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // const strArray = str
+  //   .split(' ')
+  //   .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  //   .join(' ');
+  // return strArray;
+
+  // const capitalized = [];
+  // const words = str.split(' ');
+  // words.forEach(word => {
+  //   capitalized.push(word[0].toUpperCase() + word.slice(1));
+  // });
+  // return capitalized.join(' ');
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str.charAt(i - 1) === ' ') {
+      result += str.charAt(i).toUpperCase();
+    } else {
+      result += str.charAt(i);
+    }
+  }
+  return result;
+}
 
 module.exports = capitalize;
