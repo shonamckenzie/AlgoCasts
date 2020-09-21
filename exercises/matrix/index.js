aa80e3331883eabd4d261d536486776889356450
Shona McKenzie <shona_mckenzie@hotmail.com>
2020-09-21 16:22:27 +0100

    added matrix solution

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,46 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const results = [];
+  let counter = 1;
+  let rowStart = 0;
+  let rowEnd = n - 1;
+  let columnStart = 0;
+  let columnEnd = n - 1;
 
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+
+  while (columnStart <= columnEnd && rowStart <= rowEnd) {
+    // Top row
+    for (let i = columnStart; i <= columnEnd; i++) {
+      results[rowStart][i] = counter;
+      counter ++;    
+    }
+    rowStart++;
+  // Right column
+  for (let i = rowStart; i <= rowEnd; i++) {
+    results[i][columnEnd] = counter;
+    counter++;
+  }
+  columnEnd--;
+  // Bottom row
+  for (let i = columnEnd; i >= columnStart; i--) {
+    results[rowEnd][i] = counter;
+    counter++;
+  }
+  rowEnd--;
+  // start column
+  for (let i = rowEnd; i >= rowStart; i--) {
+    results[i][columnStart] = counter;
+    counter++;
+    }
+  columnStart++;
+  }
+  return results;
+}
+
+matrix(3);
 module.exports = matrix;
